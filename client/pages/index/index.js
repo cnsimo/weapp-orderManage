dd1917320e590d4e815019133acd741cd0325a20
cnsimo <2020745751@qq.com>
2017-11-17 01:07:34 +0800

    修复主页‘添加订单’按钮无反应

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -142,7 +142,11 @@ Page({
       currentNavtab: currentNum
     });
   },
-
+  addNewOrder: function () {
+	  wx.navigateTo({
+		  url: '../newOrder/newOrder',
+	  })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
